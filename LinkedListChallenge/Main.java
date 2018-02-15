51265020dc8ecf6a42c147dd08ad7a145a363386
toktokeh <toktokeh@gmail.com>
2018-02-15 12:33:15 -0800

    Add getters and setters

--- LinkedListChallenge/Main.java
+++ LinkedListChallenge/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Song mySong = new Song("Love song", "4:17");
+        System.out.println(mySong.getTitle());
     }
 }
--- LinkedListChallenge/Song.java
+++ LinkedListChallenge/Song.java
@@ -1,9 +1,25 @@
-public Class Song {
+public class Song {
     private String title;
     private String duration;
 
-    public static Song(String title, String duration) {
+    public Song(String title, String duration) {
         this.title = title;
         this.duration = duration;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getDuration() {
+        return this.duration;
     }
 }
