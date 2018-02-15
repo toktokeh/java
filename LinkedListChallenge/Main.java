3a2b305d4fa664102c69a29748ca2c20c00cf10e
toktokeh <toktokeh@gmail.com>
2018-02-15 12:28:11 -0800

    Initial commit

---
+++ LinkedListChallenge/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Song mySong = new Song("Love song", "4:17");
+    }
+}
---
+++ LinkedListChallenge/Song.java
@@ -0,0 +1,9 @@
+public Class Song {
+    private String title;
+    private String duration;
+
+    public static Song(String title, String duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+}
