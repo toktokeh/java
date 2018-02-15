de8f6bcfacde9f672e0559e07912d43b6483e072
toktokeh <toktokeh@gmail.com>
2018-02-15 15:37:01 -0800

    Implement main()

--- LinkedListChallenge/Main.java
+++ LinkedListChallenge/Main.java
@@ -1,6 +1,70 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 public class Main {
+    
+    private static ArrayList<Album> albums = new ArrayList<Album>();
+
     public static void main(String[] args) {
-        Song mySong = new Song("Love song", 3.17);
-        System.out.println(mySong.getTitle());
+        Album album = new Album("Strombringer", "Deep Purple");
+        album.addSong("Strombringer", 4.6);
+        album.addSong("Love don't mean a thing", 4.22);
+        album.addSong("Holy man", 4.3);
+        album.addSong("Hold on", 5.6);
+        album.addSong("Lady double dealer", 3.21);
+        album.addSong("You can't do it right", 6.23);
+        album.addSong("High ball shooter", 4.27);
+        album.addSong("The gypsy", 4.2);
+        album.addSong("Soldier of fortune", 3.13);
+        albums.add(album);
+
+        album = new Album("For those about to rock", "AC/DC");
+        album.addSong("For those about to rock", 5.44);
+        album.addSong("I put the finger on you", 3.25);
+        album.addSong("Let's go", 3.45);
+        album.addSong("Inject the venom", 3.33);
+        album.addSong("Snowballed", 4.51);
+        albums.add(album);
+
+        LinkedList<Song> playList = new LinkedList<Song>();
+        albums.get(0).addToPlayList("You can't do it right", playList);
+        albums.get(0).addToPlayList("Holy man", playList);
+        albums.get(0).addToPlayList("Speed king", playList); // does not exist
+        albums.get(0).addToPlayList(9, playList);
+        albums.get(1).addToPlayList(4, playList);
+        albums.get(1).addToPlayList(10, playList); // does not exist
+
+        play(playList);
+    }
+
+    private static void play(LinkedList<Song> playList) {
+        ListIterator<Song> listIterator = playList.listIterator();
+        if (playList.size() == 0) {
+            System.out.println("No songs in play list");
+        } else {
+            System.out.println("Now playing " + listIterator.next().toString());
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
