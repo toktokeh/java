3d1aada4243c28e6a32a1802e742d7683fbd20bc
toktokeh <toktokeh@gmail.com>
2018-02-17 14:05:13 -0800

    Add healthy burger implementation

--- PolymorphismChallenge/Main.java
+++ PolymorphismChallenge/Main.java
@@ -5,5 +5,10 @@ class Main {
         hamburger.addHamburgerAddition2("Lettuce", 0.75);
         hamburger.addHamburgerAddition3("Cheese", 1.12);
         System.out.println("Total burger price is $" + hamburger.itemizeHamburger());
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+        healthyBurger.addHamburgerAddition1("Egg", 0.99);
+        healthyBurger.addHealthAddition1("Alfalfa", 0.49);
+        System.out.println("Total healthy burger price is $" + healthyBurger.itemizeHamburger());
     }
 }
